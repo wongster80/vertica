f09dcfb7f97b7bd15477e27f131ee5d2a208e734
Aidan Wong <aw110f@yp.com>
2015-11-10 00:32:23 +0000

    script to get low projectsion dependencies to determine data distribution

---
+++ get_low_projs_dependecies.sh
@@ -0,0 +1,1 @@
+for i in `/opt/vertica/bin/vsql -At  -c "Select get_node_dependencies();" | grep 'cnt\: 1$\|cnt\: 2$\|cnd\: 3$' | cut -d' ' -f1`; do /opt/vertica/bin/vsql -c "Select get_node_dependencies('${i}');" >> low_projection_dependencies.out; done
