d35ef4a6728e6c373584f5c5492a1b34979080fd
wongster80 <aidanw7@gmail.com>
2017-05-09 18:15:02 -0700

    Update get_table_usage_time.sql

--- get_table_usage_time.sql
+++ get_table_usage_time.sql
@@ -1,1 +1,1 @@
-SELECT anchor_table_schema, anchor_table_name, io_type, MAX(query_start_timestamp) last_request_timestamp FROM projection_usage GROUP BY 1, 2, 3 ORDER BY 3 DESC;
+SELECT anchor_table_schema, anchor_table_name, io_type, MAX(query_start_timestamp) last_request_timestamp FROM projection_usage GROUP BY 1, 2, 3 ORDER BY 4 DESC;
